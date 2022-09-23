0a4dd20eb232cfef2ab921bc1b3a64eac4793cd2
Edmond <34758460+EdmondtheGreat@users.noreply.github.com>
2022-09-23 16:50:36 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by OAA on 23/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
